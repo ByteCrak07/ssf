c7eb00aff410d0c5206960380615862f5b0be5b2
ByteCrak07 <abilsavio@gmail.com>
2022-04-28 17:17:55 +0530

    Add adorn form

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,71 +1,3 @@
-const form = document.getElementById("event_form");
-if (form) {
-  form.addEventListener("submit", formSubmit);
-
-  function formSubmit(e) {
-    e.preventDefault();
-
-    const inputs = form.getElementsByTagName("input");
-
-    var data = {};
-
-    for (var i = 0; i < inputs.length; i++) {
-      data[inputs[i].name] = inputs[i].value;
-    }
-
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    var raw = JSON.stringify(data);
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
-
-    //   fetch(
-    //     "https://hook.eu1.make.com/bhsxp34m1aeg1iicyyo7lqdkdm393nhn/",
-    //     requestOptions
-    //   )
-    //     .then(function (response) {
-    //       response.text();
-    //     })
-    //     .then(function (result) {
-    //       console.log(result);
-    //     })
-    //     .catch(function (error) {
-    //       console.log("error", error);
-    //     });
-
-    data["time"] = { ".sv": "timestamp" };
-
-    var raw = JSON.stringify(data);
-
-    var requestOptions = {
-      method: "POST",
-      headers: myHeaders,
-      body: raw,
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://summer-startup-festival-default-rtdb.asia-southeast1.firebasedatabase.app/chosen_sub.json",
-      requestOptions
-    )
-      .then(function (response) {
-        return response.text();
-      })
-      .then(function (result) {
-        document.getElementById("form_success").classList.remove("sr-only");
-      })
-      .catch(function (error) {
-        document.getElementById("form_error").classList.remove("sr-only");
-      });
-  }
-}
-
 // Campus ambassador form
 const ambassadorForm = document.getElementById("ambassador_form");
 if (ambassadorForm) {
@@ -73,6 +5,8 @@ if (ambassadorForm) {
 
   function ambassadorFormSubmit(e) {
     e.preventDefault();
+    const submitBtn = ambassadorForm.getElementsByTagName("button")[0];
+    submitBtn.disabled = true;
 
     const inputs = ambassadorForm.getElementsByTagName("input");
 
@@ -116,6 +50,70 @@ if (ambassadorForm) {
         document.getElementById("form_success").classList.remove("sr-only");
       })
       .catch(function (error) {
+        submitBtn.disabled = false;
+        document.getElementById("form_error").classList.remove("sr-only");
+      });
+  }
+}
+
+// Adorn form
+const adornForm = document.getElementById("adorn_form");
+if (adornForm) {
+  adornForm.addEventListener("submit", adornFormSubmit);
+
+  function adornFormSubmit(e) {
+    e.preventDefault();
+    const submitBtn = adornForm.getElementsByTagName("button")[0];
+    submitBtn.disabled = true;
+
+    const inputs = adornForm.getElementsByTagName("input");
+    const textAreas = adornForm.getElementsByTagName("textarea");
+
+    var data = {};
+
+    for (var i = 0; i < inputs.length; i++) {
+      data[inputs[i].name] = inputs[i].value;
+    }
+
+    for (var i = 0; i < inputs.length; i++) {
+      if (textAreas[i]) data[textAreas[i].name] = textAreas[i].value;
+    }
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    var raw = JSON.stringify(data);
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    data["time"] = { ".sv": "timestamp" };
+
+    var raw = JSON.stringify(data);
+
+    var requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://summer-startup-festival-default-rtdb.asia-southeast1.firebasedatabase.app/adorn.json",
+      requestOptions
+    )
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (result) {
+        document.getElementById("form_success").classList.remove("sr-only");
+      })
+      .catch(function (error) {
+        submitBtn.disabled = false;
         document.getElementById("form_error").classList.remove("sr-only");
       });
   }
